eb552f1d0b019e823a548161f0fe9603fbde9831
Sakura <845888325@qq.com>
2021-06-30 18:03:31 +0800

    debug theme

--- market/src/app/core/core.module.ts
+++ market/src/app/core/core.module.ts
@@ -1,11 +1,18 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { throwError } from 'rxjs';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpTokenInterceptor } from './interceptors';
+import { JwtService } from './services';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:HttpTokenInterceptor,multi:true},
+    JwtService
   ]
 })
 export class CoreModule { 
